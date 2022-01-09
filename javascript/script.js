eeef06c498ae6448effc0955d7fcbb7ba7c94130
Kain Nhantumbo <nhantumbok@gmail.com>
2022-01-09 16:54:36 +0200

    Adicionado o formulário de cadastro

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,39 @@
-//mostra ou esconde o menu
+
 (function () {
+    //mostra ou oculta o menu
     const toogleMenu = () => {
         document.querySelector('.nav-items').classList.toggle('hide-menu');
     }
+
+    //mostra ou oculta o modal de login
+    const login = () => {
+        document.querySelector('.modal-form').classList.toggle('hide-items');
+        toogleMenu();
+    }
+
+    //remove o modal de login
+    const quitModal = () => {
+        document.querySelector('.modal-form').classList.add('hide-items');
+    }
+
+    document.querySelector('.btn-quit').addEventListener('click', quitModal)
+    document.querySelector('#btn-login').addEventListener('click', login);
     document.querySelector('.btn-menu').addEventListener('click', toogleMenu);
+})();
+
+
+
+//login do usuário
+(function () {
+    const btnEntrar = document.querySelector('.btn-entrar');
+    const email = document.querySelector('#email-input').value;
+    const password = document.querySelector('#password-input').value;
+
+    const userData = () => {
+
+        
+
+    }
+
+    btnEntrar.addEventListener('click', userData)
 })();
